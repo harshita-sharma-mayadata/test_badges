5c85a4e261842c6270323332303d67f78b6c7396
harshita-sharma-mayadata <48346382+harshita-sharma-mayadata@users.noreply.github.com>
2019-03-13 15:32:54 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,4 +7,9 @@ def test_answer():
 	y = 2
 	if inc(3) == 5: 
 		y = y + 1
+#git push https://username:password@myrepository.biz/file.git --all
+with open("README.md", "a") as myfile:
+    myfile.write("appended text")
+
+
 	
