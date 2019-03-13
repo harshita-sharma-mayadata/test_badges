5bae2a9caeaf71288e3165391d93e1339d218bb3
harshita-sharma-mayadata <harshita.sharma@mayadata.io>
2019-03-13 17:17:56 +0530

    python
    Signed-off-by: harshita-sharma-mayadata <harshita.sharma@mayadata.io>

---
+++ git.py
@@ -0,0 +1,12 @@
+#git push https://username:password@myrepository.biz/file.git --all
+import os
+with open("/home/mayadata/Desktop/badges/README.md", "a") as myfile:
+    myfile.write("update text")
+
+
+cmd = "git add README.md"
+cmd1 = "git commit -s -m {}".format("update")
+cmd2 = "git push origin"
+os.system(cmd)
+os.system(cmd1)
+os.system(cmd2)
